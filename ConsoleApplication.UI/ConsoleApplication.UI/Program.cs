89901da667d5f1d8bc671dae3d515dd103656d8b
John Doe <john@sample.com>
2017-06-14 14:54:46 +0200

    hellur

---
+++ ConsoleApplication.UI/ConsoleApplication.UI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkExample.Helper;
+using MarkExample.FileHelper;
+
+namespace ConsoleApplication.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper helper = new Helper();
+            string greeting = helper.GreetUser("Mark", "Barfoot");
+
+            helper.DoSomething();
+
+            Console.WriteLine("\n");
+            Console.WriteLine(Environment.NewLine);
+
+            FileReader fileReader = new FileReader();
+            fileReader.ReadFile("Animals.txt");
+
+            Console.WriteLine(greeting);
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("You pressed Enter");
+                Console.ReadKey();
+            }
+        }
+    }
+}
