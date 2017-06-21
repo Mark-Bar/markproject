cc00901cba80e60f7181f9dcc0f4183a96eb3681
John Doe <john@sample.com>
2017-06-21 12:57:11 +0200

    send home

---
+++ FirstSqlApp/FirstSqlApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FirstSqlApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvJobTitles = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtJobTitle = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJobTitles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvJobTitles
+            // 
+            this.dgvJobTitles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJobTitles.Location = new System.Drawing.Point(12, 153);
+            this.dgvJobTitles.Name = "dgvJobTitles";
+            this.dgvJobTitles.Size = new System.Drawing.Size(258, 150);
+            this.dgvJobTitles.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.Location = new System.Drawing.Point(195, 51);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Job Title";
+            // 
+            // txtJobTitle
+            // 
+            this.txtJobTitle.Location = new System.Drawing.Point(110, 25);
+            this.txtJobTitle.Name = "txtJobTitle";
+            this.txtJobTitle.Size = new System.Drawing.Size(160, 20);
+            this.txtJobTitle.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 362);
+            this.Controls.Add(this.txtJobTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvJobTitles);
+            this.Name = "Form1";
+            this.Text = "Job Titles";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJobTitles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvJobTitles;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtJobTitle;
+    }
+}
+
---
+++ FirstSqlApp/FirstSqlApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace FirstSqlApp
+{
+    public partial class Form1 : Form
+    {
+        string sqlConnection = ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.LoadJobTitles();
+        }
+
+        private void LoadJobTitles()
+        {
+            string commandText = "select top 10 * from [dbo].[JobTitle] order by Id desc";
+
+            using (var con = new SqlConnection(sqlConnection))
+            {
+                using (var command = new SqlCommand(commandText, con))
+                {
+                    con.Open();
+
+                    DataTable jobTitles = new DataTable();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(jobTitles);
+
+                    dgvJobTitles.DataSource = jobTitles;
+                    con.Close();
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string jobTitle = txtJobTitle.Text;
+
+            if (!String.IsNullOrEmpty(jobTitle))
+            {
+                string commandText = "insert into [dbo].[JobTitle](JobTitle) values(@jobTitle)";
+
+                using (var con = new SqlConnection(sqlConnection))
+                {
+                    using (var command = new SqlCommand(commandText, con))
+                    {
+                        con.Open();
+
+                        command.Parameters.AddWithValue("jobTitle", jobTitle);
+                        command.ExecuteNonQuery();
+
+                        LoadJobTitles();
+
+                        con.Close();
+                    }
+                }
+            }
+        }
+    }
+}
